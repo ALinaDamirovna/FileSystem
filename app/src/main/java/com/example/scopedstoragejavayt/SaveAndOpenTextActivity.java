3e77489e64a5fd7330b0410d9d486783238a18d4
ALinaDamirovna <alinadamirovna13@gmail.com>
2022-05-26 21:18:10 +0300

    SecondCommit

--- app/src/main/java/com/example/scopedstoragejavayt/SaveAndOpenTextActivity.java
+++ app/src/main/java/com/example/scopedstoragejavayt/SaveAndOpenTextActivity.java
@@ -1,6 +1,7 @@
 package com.example.scopedstoragejavayt;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,7 +16,8 @@ import java.io.IOException;
 
 public class SaveAndOpenTextActivity extends AppCompatActivity {
 
-    private final static String FILE_NAME = "document.txt";
+    private final static String FILE_NAME = "document2.txt";
+    private final static String FILE_NAME1 = "content2.txt";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +25,91 @@ public class SaveAndOpenTextActivity extends AppCompatActivity {
     }
     //получаем доступ к папке приложения во внешнем хранилище и устанавливаем объект файла:
     private File getExternalPath() {
-        return new File(getExternalFilesDir(null), FILE_NAME);
+        return new File(getExternalFilesDir(null), FILE_NAME1);
     }
-    // сохранение файла
+    // сохранение файла на SD
     public void saveText(View view){
+        if(!isExternalStorageWritable()) return;
+        if (!checkSpace()){
+            Toast.makeText(this, "Осталось менее 500 МБ", Toast.LENGTH_SHORT).show();
+        } else{
+            try(FileOutputStream fos = new FileOutputStream(getExternalPath())) {
+                EditText textBox = findViewById(R.id.editor);
+                String text = textBox.getText().toString();
+                fos.write(text.getBytes());
+                Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
+            }
+            catch(IOException ex) {
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+
+    // открытие файла SD
+    public void openText(View view){
+
+        TextView textView = findViewById(R.id.text);
+        File file = getExternalPath();
+        // если файл не существует, выход из метода
+        if(!file.exists() || !isExternalStorageReadable()) return;
+        try(FileInputStream fin =  new FileInputStream(file)) {
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String (bytes);
+            textView.setText(text);
+        }
+        catch(IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //РАБОТА С SD
+    //проверка на возможность чтения и записи с/на SD
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteFileSD(View view){
+        File file = getExternalPath();
+        file.delete();
+    }
+
+    public void deleteFile(View view){
+        File dir = getFilesDir();
+        File file = new File(dir,FILE_NAME1);
+        file.delete();
+    }
+
+    public boolean checkSpace(){
+        File dir = new File(String.valueOf(getExternalFilesDir(null)));
+        System.out.println("МЕСТО total "+ dir.getTotalSpace()/(1024*1024));
+        System.out.println("МЕСТО usable "+ dir.getUsableSpace()/(1024*1024));
+        System.out.println(dir.getTotalSpace()/(1024*1024)-dir.getUsableSpace()/(1024*1024));
+        if ((dir.getTotalSpace()/(1024*1024)-dir.getUsableSpace()/(1024*1024))>200)
+            return true;
+        else
+            return false;
+    }
+
+    // сохранение файла
+    public void saveText1(View view){
 
         try(FileOutputStream fos = new FileOutputStream(getExternalPath())) {
             EditText textBox = findViewById(R.id.editor);
@@ -40,7 +123,7 @@ public class SaveAndOpenTextActivity extends AppCompatActivity {
         }
     }
     // открытие файла
-    public void openText(View view){
+    public void openText1(View view){
 
         TextView textView = findViewById(R.id.text);
         File file = getExternalPath();
